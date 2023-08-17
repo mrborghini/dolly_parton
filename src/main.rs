8a21ae78aecb2bcf366d2e716572c04fee796880
lamborghinigamer1 <lamborghinigamermobiel@gmail.com>
2023-08-17 12:08:10 +0200

    Added random agent generator

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,17 @@ fn random_number(startrange: usize, maxrange: usize) -> usize {
     return num;
 }
 
+fn format_agents(agents: &[&str]) -> String {
+    let mut formatted_agents = String::new();
+
+    for (index, agent) in agents.iter().enumerate() {
+        let player = format!("**Player {}**:", index + 1);
+        formatted_agents.push_str(&format!("{} {}\n", player, agent));
+    }
+
+    formatted_agents
+}
+
 fn createdb(
     database_name: &str,
     username: &str,
@@ -124,6 +135,7 @@ impl EventHandler for Handler {
                     "!gosleep",
                     "!silly",
                     "!valgun",
+                    "!valagents",
                     "!rizz",
                 ];
                 let response = format!("**Commands:**\n{}", commands.join("\n"));
@@ -140,6 +152,48 @@ impl EventHandler for Handler {
                     println!("Error sending message: {:?}", why);
                 }
             }
+
+            "!valagents" => {
+
+            let mut selectedagents: Vec<&str> = Vec::new();
+
+            let agents = [
+                "Astra",
+                "Breach", 
+                "Brimstone", 
+                "Chamber", 
+                "Cypher", 
+                "Deadlock", 
+                "Fade", 
+                "Gekko", 
+                "Harbor", 
+                "Jett", 
+                "Kay/O", 
+                "Killjoy", 
+                "Neon", 
+                "Omen", 
+                "Phoenix", 
+                "Raze", 
+                "Reyna", 
+                "Sage", 
+                "Skye", 
+                "Sova", 
+                "Viper", 
+                "Yoru"
+                ];
+            
+            while selectedagents.len() < 5 {
+                let agent = agents[random_number(0, agents.len() -1)];
+                if !selectedagents.contains(&agent) {
+                    selectedagents.push(agent);
+                }
+            }
+            if let Err(why) = msg.channel_id.say(&ctx.http, format_agents(&selectedagents)).await {
+                println!("Error sending message: {:?}", why);
+            }            
+
+            }
+            
 
             "!cal" => {
                 if splitcommand.len() > 1 {
@@ -334,7 +388,7 @@ impl EventHandler for Handler {
 
             "!valgun" => {
                 let valguns = [
-                    "Classic", "Shorty", "Frenzy", "Ghost", "Sherrif", "Stinger", "Spectre",
+                    "Classic", "Shorty", "Frenzy", "Ghost", "Sherriff", "Stinger", "Spectre",
                     "Bucky", "Judge", "Bulldog", "Guardian", "Phantom", "Vandal", "Marshal",
                     "Operator", "Ares", "Odin",
                 ];
