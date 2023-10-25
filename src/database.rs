9c4b61979dbcef3a154567a5852a947c951e78e5
lamborghinigamer1 <61702082+lamborghinigamer1@users.noreply.github.com>
2023-10-25 21:41:26 +0200

    Fixed social credits not updating properly

--- src/database.rs
+++ src/database.rs
@@ -42,17 +42,30 @@ pub fn getuserinfo(user: &str) -> Result<Option<(String, i32)>, mysql::Error> {
     }
 }
 
-pub fn add_credits(user: &str, credits: i32) -> Result<Option<(String, i32)>, mysql::Error> {
+pub fn add_credits(user: &str, new_credits: i32) -> Result<Option<(String, i32)>, mysql::Error> {
     let pool = establish_connection()?;
     let mut conn = pool.get_conn()?;
 
-    let stmt = conn.prep("UPDATE social_credits SET credits = ? WHERE user = ?")?;
-    let mut rows = conn.exec_iter(&stmt, (credits, user,))?;
+    // Step 1: Get the current credits for the user
+    let select_stmt = conn.prep("SELECT user, credits FROM social_credits WHERE user = ?")?;
+    let mut select_rows = conn.exec_iter(&select_stmt, (user,))?;
 
-    if let Some(row) = rows.next() {
-        let (username, credits) = from_row::<(String, i32)>(row?);
-        Ok(Some((username, credits)))
+    if let Some(row) = select_rows.next() {
+        let (username, current_credits) = from_row::<(String, i32)>(row?);
+
+        // Release the mutable borrow on `conn` by dropping select_rows
+        drop(select_rows);
+
+        // Step 2: Calculate the new total credits
+        let total_credits = current_credits + new_credits;
+
+        // Step 3: Update the database with the new total credits
+        let update_stmt = conn.prep("UPDATE social_credits SET credits = ? WHERE user = ?")?;
+        conn.exec_drop(&update_stmt, (total_credits, user))?;
+
+        Ok(Some((username, total_credits)))
     } else {
+        // User not found, return None
         Ok(None)
     }
 }
