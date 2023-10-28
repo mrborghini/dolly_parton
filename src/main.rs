1e4854091c863097020498b9e245204d3d149eaa
lamborghinigamer1 <61702082+lamborghinigamer1@users.noreply.github.com>
2023-10-28 14:55:28 +0200

    Changed all the commands to guild commands

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use dotenv::dotenv;
 use serenity::async_trait;
-use serenity::model::application::command::Command;
+// use serenity::model::application::command::Command;
 use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
@@ -109,43 +109,54 @@ impl EventHandler for Handler {
                 .expect("GUILD_ID must be an integer"),
         );
 
-        // let commands = GuildId::set_application_commands(&_guild_id, &ctx.http, |commands| {
-        //     commands
-        //         .create_application_command(|command| commands::ping::register(command))
-        //         .create_application_command(|command| commands::work::register(command))
-        //         .create_application_command(|command| commands::socialcredits::register(command))
-        //         .create_application_command(|command| commands::cal::register(command))
-        // })
-        // .await;
-
-        // println!(
-        //     "I now have the following guild slash commands: {:#?}",
-        //     commands
-        // );
-
-        let _guild_command = Command::create_global_application_command(&ctx.http, |command| {
-            commands::wonderful_command::register(command);
-            commands::ping::register(command);
-            commands::dollyhelp::register(command);
-            commands::work::register(command);
-            commands::dolly::register(command);
-            commands::valagents::register(command);
-            commands::valgun::register(command);
-            commands::quote::register(command);
-            commands::compliment::register(command);
-            commands::daddy::register(command);
-            commands::gosleep::register(command);
-            commands::rage::register(command);
-            commands::socialcredits::register(command);
-            commands::rizz::register(command);
-            commands::cal::register(command)
+        let commands = GuildId::set_application_commands(&_guild_id, &ctx.http, |commands| {
+            commands
+                .create_application_command(|command| commands::ping::register(command))
+                .create_application_command(|command| commands::work::register(command))
+                .create_application_command(|command| commands::socialcredits::register(command))
+                .create_application_command(|command| commands::wonderful_command::register(command))
+                .create_application_command(|command| commands::dollyhelp::register(command))
+                .create_application_command(|command| commands::dolly::register(command))
+                .create_application_command(|command| commands::valagents::register(command))
+                .create_application_command(|command| commands::valgun::register(command))
+                .create_application_command(|command| commands::quote::register(command))
+                .create_application_command(|command| commands::compliment::register(command))
+                .create_application_command(|command| commands::daddy::register(command))
+                .create_application_command(|command| commands::gosleep::register(command))
+                .create_application_command(|command| commands::rage::register(command))
+                .create_application_command(|command| commands::rizz::register(command))
+                .create_application_command(|command| commands::cal::register(command))
         })
         .await;
 
         println!(
-            "I created the following global slash command: {:#?}",
-            _guild_command
+            "I now have the following guild slash commands: {:#?}",
+            commands
         );
+
+        // let _guild_command = Command::create_global_application_command(&ctx.http, |command| {
+        //     commands::wonderful_command::register(command);
+        //     commands::ping::register(command);
+        //     commands::dollyhelp::register(command);
+        //     commands::work::register(command);
+        //     commands::dolly::register(command);
+        //     commands::valagents::register(command);
+        //     commands::valgun::register(command);
+        //     commands::quote::register(command);
+        //     commands::compliment::register(command);
+        //     commands::daddy::register(command);
+        //     commands::gosleep::register(command);
+        //     commands::rage::register(command);
+        //     commands::socialcredits::register(command);
+        //     commands::cal::register(command);
+        //     commands::rizz::register(command)
+        // })
+        // .await;
+
+        // println!(
+        //     "I created the following global slash command: {:#?}",
+        //     _guild_command
+        // );
     }
 }
 
