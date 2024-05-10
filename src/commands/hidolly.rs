d2446f4cb2491d425bc419882e2a3c351c067df1
lamborghinigamer1 <61702082+lamborghinigamer1@users.noreply.github.com>
2024-05-11 01:06:29 +0200

    Fixed dolly cloning previous response

--- src/commands/hidolly.rs
+++ src/commands/hidolly.rs
@@ -41,12 +41,21 @@ async fn get_ai_message(message: String, base_url: String) -> Result<String, Box
         }
 
         if let Some(ctx) = response["context"].as_array() {
+            let mut allowed: Vec<i32> = Vec::new();
             let ctx_i32: Vec<i32> = ctx
                 .iter()
                 .filter_map(|v| v.as_i64())
                 .map(|v| v as i32)
                 .collect();
-            let _ = _add_context_to_dolly_ai(&ctx_i32);
+
+            for &item in ctx_i32.iter() {
+                if !contexts.contains(&item) && !allowed.contains(&item) {
+                    allowed.push(item);
+                    println!("Added {}", item);
+                }
+            }
+
+            let _ = _add_context_to_dolly_ai(&allowed);
         }
     }
 
